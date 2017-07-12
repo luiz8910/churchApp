09be93c83b666a045c5c58d9290ff48db9682da9
Luiz <luiz.sanches8910@gmail.com>
2017-07-12 01:16:59 -0300

    lista dia 10 e 11

---
+++ resources/views/includes/calendar.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luiz
+ * Date: 10/07/2017
+ * Time: 19:39
+ */
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luiz
+ * Date: 10/07/2017
+ * Time: 15:19
+ */
