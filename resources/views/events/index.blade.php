2ad0f9e8bdbf1d903c715fd4dc04e42dc8cb055b
Luiz <luiz.sanches8910@gmail.com>
2017-11-29 00:29:25 -0200

    bug nome do autor de eventos 11

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -387,7 +387,7 @@ License: You must have a valid license purchased only from themeforest(the above
                                                             </table>
                                                             <br>
                                                             <div class="pull-right">
-                                                                {{ $events_pag->links() }}
+                                                                {{ $events->links() }}
                                                             </div>
 
                                                             <div class="progress" id="progress-danger" style="display: none;">
